2e632acf7389077b7041447b6f79da41d579e226
Magesh Kuppan <tkmagesh77@gmail.com>
2021-08-06 12:26:25 +0530

    using 'useSelector' & 'useDispatch'

--- bug-tracker-app/src/bugs/components/bug-list.jsx
+++ bug-tracker-app/src/bugs/components/bug-list.jsx
@@ -1,9 +1,14 @@
 import BugItem from './bug-item';
 import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux'
 import bugActionCreators from '../actions'
 
-const BugList = ({bugs, toggle, remove, removeClosed}) => {
+const BugList = () => {
+    const dispatch = useDispatch();
+    const bugs = useSelector(state => state.bugState);
+   
+    const { toggle, remove, removeClosed } = bindActionCreators(bugActionCreators, dispatch);
     const bugList = bugs.map(bug => (
         <BugItem key={bug.id} {...{bug, toggle, remove}} />
     ));
@@ -17,7 +22,10 @@ const BugList = ({bugs, toggle, remove, removeClosed}) => {
     )
 }
 
-export default connect(
-    storeState =>({bugs : storeState.bugState}) /* mapStateToProps */,
-    dispatch => bindActionCreators(bugActionCreators, dispatch) /* mapDispatchToProps */
-)(BugList);
+/* export default connect(
+    storeState =>({bugs : storeState.bugState}) ,
+    dispatch => bindActionCreators(bugActionCreators, dispatch) 
+)(BugList); 
+*/
+
+export default BugList;
