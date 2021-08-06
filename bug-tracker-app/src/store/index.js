5ceb5b9896e4fd205c1f846520e3aeb2ccd866e0
Magesh Kuppan <tkmagesh77@gmail.com>
2021-08-06 13:00:23 +0530

    middleware introduction

--- bug-tracker-app/src/store/index.js
+++ bug-tracker-app/src/store/index.js
@@ -1,13 +1,50 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
 import bugsReducer from '../bugs/reducers/bugsReducer';
 import projectsReducer from '../projects/reducers/projectsReducer'
+
+/* function loggerMiddleware(store){
+    console.log('[@loggerMiddleware] triggered')
+    return function(next){
+        console.log('[@loggerMiddleware] next middleware configured with next -> ', next.name)
+        return function(action){
+            console.log('[@loggerMiddleware] action dispatched -> ', action);
+            console.group(action.type);
+            console.log('Before ', store.getState())
+            console.log(action)
+            next(action)
+            console.log('After ', store.getState())
+            console.groupEnd();
+        }
+    }
+}
+
+function dummyMiddleware(store){
+    console.log('[@dummyMiddleware] triggered')
+    return function dummyNext(next){
+        console.log('[@dummyMiddleware] next middleware configured with next -> ', next.name)
+        return function dummyNext(action){
+            console.log('[@dummyMiddleware] action dispatched -> ', action);
+            next(action)
+        }
+    }
+} */
+
+const loggerMiddleware = store => next => action => {
+    console.group(action.type);
+    console.log('Before ', store.getState())
+    console.log('Action ', action)
+    next(action)
+    console.log('After ', store.getState())
+    console.groupEnd();
+};
+
 
 const rootReducer = combineReducers({
     bugState :  bugsReducer,
     projectState : projectsReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 export default store;
